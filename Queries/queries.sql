4e886394ee213cb5e320cdb5168dd996ced6368c
penlow99 <penlow99@yahoo.com>
2020-10-22 17:27:12 -0500

    queries added for retirement info

---
+++ Queries/queries.sql
@@ -0,0 +1,22 @@
+-- Retirement elgibility years query
+SELECT first_name, last_name 
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Number of employees eligible for retirement
+SELECT COUNT (first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- Create table of employees eligible for retirement, used to export .csv file
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
